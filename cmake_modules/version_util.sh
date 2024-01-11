27bffa577bd761615bee7eae9aa4f51d5f0cd2e1
Suma Hegde <suma.hegde@amd.com>
2024-01-11 10:30:32 +0000

    cmake: Modify package and library versioning
    There is no need for maintaining separate package and
    library(shared object) versioning.
    Hence modify CMake file to use only one version.
    The versioning is done through git annotated tags.
    Tag name should start with "esmi_pkg_ver-x.x.x"
    Version field contains major, minor numbers and patch level.
    
    The generated shared library name uses only major, minor and patch levels.
    Generated package(deb/rpm) will have additional 2 fields along with these 3 fields.
    One field indicates number of commits between current tag and previous
    tag. Another field is provided for the user to pass any custom string
    through cmake -D option.
    cmake ../ -D ESMI_BUILD_ID="test-build".
    If -D option is not provided, then default string "local-build-0"
    is appended to the package name.
    
    Change-Id: I71b4453e46447747a9bd69b848f69077b44f5200

--- cmake_modules/version_util.sh
+++ cmake_modules/version_util.sh
@@ -16,7 +16,8 @@ TAG_PREFIX=$1
 reg_ex="${TAG_PREFIX}*"
 
 commits_since_last_tag() {
-  TAG_ARR=(`git tag --sort=committerdate -l ${reg_ex} | tail -2`)
+  GIT_BRANCH=(`git branch --show-current`)
+  TAG_ARR=(`git tag --sort=committerdate --merged ${GIT_BRANCH} -l ${reg_ex} | tail -2`)
   PREVIOUS_TAG=${TAG_ARR[1]}
   CURRENT_TAG=${TAG_ARR[0]}
   NUM_COMMITS=0
