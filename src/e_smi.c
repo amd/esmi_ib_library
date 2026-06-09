4b2eefbfadb6a56bb42f1df1bc34db1f7393984f
Muthusamy Ramalingam <muthusamy.ramalingam@amd.com>
2026-06-09 08:53:38 +0000

    lib: Support to get socketCount using ceiling division
    Added support to ceiling division method to get socketCount
    
    Signed-off-by: Muthusamy Ramalingam <muthusamy.ramalingam@amd.com>

--- src/e_smi.c
+++ src/e_smi.c
@@ -374,7 +374,10 @@ static esmi_status_t detect_packages(struct system_metrics *psm)
 		return ESMI_IO_ERROR;
 
 	/* Number of sockets in the system */
-	psm->total_sockets = psm->total_cores / max_cores_socket;
+	psm->total_sockets = (psm->total_cores + max_cores_socket - 1)/ max_cores_socket;
+
+	if (psm->total_sockets == 0)
+		return ESMI_NOT_SUPPORTED;
 
 	return ESMI_SUCCESS;
 }
